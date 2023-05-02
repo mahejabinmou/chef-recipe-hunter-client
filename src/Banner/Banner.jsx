002c3d3debedd50410dd672a41e59c99365aabfb
Mou <mahejabinmou.99@gmail.com>
2023-05-02 05:15:56 -0700

    login done

--- src/Banner/Banner.jsx
+++ src/Banner/Banner.jsx
@@ -4,8 +4,8 @@ const Banner = () => {
     return (
         <div className='md:flex justify-center items-center w-full mt-4'>
         <div className='w-64 px-4'>
-        <h4 className='text-4xl'>One Step Closer To Your <br /> <span className='text-indigo-500 text-4xl'>Dream Job</span></h4>
-        <p><small>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</small></p>
+        <h4 className='text-4xl'>Speciall Food Every Time For You and <br /> <span className='text-indigo-500 text-4xl'>The Fastest Delivery In Your City</span></h4>
+        <p><small>Be the fastest in delivering your healthy food and with us order your best food anytime and anywher</small></p>
          <button className='bg-indigo-500 rounded p-2 text-white'>Get Started</button> 
         </div>
 
