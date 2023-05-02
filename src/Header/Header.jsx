20157a2ad42a1925467836fe4aa6033869f50e98
Mou <mahejabinmou.99@gmail.com>
2023-05-01 19:01:39 -0700

    firebase added

---
+++ src/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+// import ActiveLink from '../ActiveLink/ActiveLink';
+
+const Header = () => {
+    return (
+        <div></div>
+    );
+};
+
+export default Header;
---
+++ src/firebase/firebase.config.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBYfoP6upcSRG_i_2-tucVeD2vF1i3WGLM",
+  authDomain: "assignment-10-client-sid-46979.firebaseapp.com",
+  projectId: "assignment-10-client-sid-46979",
+  storageBucket: "assignment-10-client-sid-46979.appspot.com",
+  messagingSenderId: "380823715501",
+  appId: "1:380823715501:web:39ad1ca53b6ce35cfa5024"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
