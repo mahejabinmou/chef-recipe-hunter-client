eb3e764afb226f729df2a6ecf376c95aeb73c656
Mou <mahejabinmou.99@gmail.com>
2023-05-04 02:03:59 -0700

    lazy load added

--- src/Chef/Chef.jsx
+++ src/Chef/Chef.jsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 // import { Link } from 'react-router-dom';
+import LazyLoad from 'react-lazy-load';
 
 const Chef = () => {
     const [chefsData, setChefsData] = useState([]);
@@ -20,7 +21,10 @@ const Chef = () => {
                 {
                     chefsData.map((chef) => (
                         <div key={chef.id} className="max-w-sm p-4 mb-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                            
+                            <LazyLoad height={400} width={350}>
                             <img src={chef.image} alt="" />
+    </LazyLoad>
                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{chef.name}</h5>
 
                             <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Eperience: {chef.experience}</p>
@@ -30,6 +34,7 @@ const Chef = () => {
                             <Link to={`/chefRecepie/${chef.id}`}>
                                 <button className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">View Recepie</button>
                             </Link>
+                           
 
                         </div>
 
