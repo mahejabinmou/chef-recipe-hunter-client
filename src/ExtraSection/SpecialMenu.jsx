30bb6cccd91dce29e34abe4472e5812208321ae6
Mou <mahejabinmou.99@gmail.com>
2023-05-05 09:41:52 -0700

    extra section updated

--- src/ExtraSection/SpecialMenu.jsx
+++ src/ExtraSection/SpecialMenu.jsx
@@ -3,8 +3,9 @@ import React from 'react';
 const SpecialMenu = () => {
     return (
         <div className='text-center my-12 p-4'>
-            <h2 className='font-bold text-4xl'>Special Chef Menu</h2>
-            <p>Browse our special chef menu and let us create something knew for you </p>
+            <h2 className='font-bold m-4 text-4xl'>Special Chef Menu</h2>
+            <p>Indian cuisine consists of a variety of regional and traditional cuisines native to India. Given the diversity in soil, climate, culture, ethnic groups, and occupations, these cuisines vary substantially and use locally available spices, herbs, vegetables, and fruits.
+            Browse our special chef menu and let us create something knew for you </p>
         </div>
     );
 };
